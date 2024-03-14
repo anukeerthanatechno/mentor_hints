f09cec422c2e28703000fb59bdbf6ae115595af7
anukeerthy18 <anukeerthanasomu@gmail.com>
2024-03-14 22:54:43 +0530

    Navbar and footer design

--- src/layout/footer/Footer.js
+++ src/layout/footer/Footer.js
@@ -1,8 +1,63 @@
 import React from 'react'
+import logo from '../../assets/logo/logo.png'
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Logooutline from "../../assets/images/logooutline.png"
+import CopyrightIcon from '@mui/icons-material/Copyright';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <>        <div className='flex justify-between mt-20 px-12'>
+            <div>
+                <div className='logo mb-6'>
+                    <img src={logo} alt='logo' className='w-36' />
+                </div>
+                <div className='p-4 flex gap-2'>
+                    <div className='facbook_colour'>
+                        <FacebookRoundedIcon />
+                    </div>
+                    <div className='twitter_icon'>
+                        <TwitterIcon />
+                    </div>
+                    <div className='instagram_icon'>
+                        <InstagramIcon />
+                    </div>
+                    <div className='linkedin_icon'>
+                        <LinkedInIcon />
+                    </div>
+                </div>
+            </div>
+            <div className='flex gap-10'>
+                <div>
+                    <a href="" className='py-2 text-sm'>HOW IT WORK</a>
+                </div>
+                <div className='flex flex-col'>
+                    <a href="" className='py-1 text-sm'>FAQ</a>
+                    <a href="" className='py-2 text-sm'>PRIVACY POLICY</a>
+                    <a href="" className='py-2 text-sm'>TERMS AND CONDITION</a>
+                </div>
+                <div>
+                    <a href="" className='py-2 text-sm'>BLOG</a>
+                </div>
+                <div>
+                    <a href="" className='py-2 text-sm'>ARE YOU HIRING? <span style={{ color: '#FF914D' }}>FIND TALENT </span></a>
+                </div>
+            </div>
+        </div>
+            <div className='flex justify-center items-center'>
+                <img src={Logooutline} alt='outline' className='w-80 items-center justify-center' />
+            </div>
+            <div className='flex justify-center'>
+                <div className='text-black'>
+                    <CopyrightIcon />
+                </div>
+                <div>
+                    <p className='text-black'>2024 Mentor Hint  Inc. All rights reserved </p>
+                </div>
+            </div>
+        </>
     )
 }
 
--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -1,188 +1,64 @@
-import { AppBar, CssBaseline, IconButton, Typography, Box, Button, Toolbar, Drawer, ListItem, ListItemButton, Divider, List } from '@mui/material'
+import React, { useState } from 'react'
+import logo from '../../assets/logo/logo.png'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import MenuIcon from '@mui/icons-material/Menu';
-import React from 'react'
-import ListItemText from '@mui/material/ListItemText';
-
-const Header = (props) => {
-    const drawerWidth = 240;
-    const navItems = ['Home', 'About', 'Tech Network', 'Blog', 'Contact'];
-    const { window } = props;
-    const [mobileOpen, setMobileOpen] = React.useState(false);
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 
 
-    const handleDrawerToggle = () => {
-        setMobileOpen((prevState) => !prevState);
-    };
 
-    const drawer = (
-        <Box onClick={handleDrawerToggle} sx={{ textAlign: 'left' }}>
-            <Typography variant="h6" sx={{ my: 2, marginLeft: 2 }}>
-                Logo
-            </Typography>
-            <Divider />
-            <List>
-                {navItems.map((item) => (
-                    <ListItem key={item} disablePadding>
-                        <ListItemButton sx={{ textAlign: 'Left' }}>
-                            <ListItemText primary={item} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
-        </Box>
-    );
+const Header = () => {
+    const links = [
+        { name: 'Home', link: '/home' },
+        { name: 'About', link: '/about' },
+        { name: 'Tech network', link: '/technetwork' },
+        { name: 'Blog', link: '/blog' },
+        { name: 'Contact', link: '/contact' },
+    ]
 
-    const container = window !== undefined ? () => window().document.body : undefined;
-
+    const [isOpen, setIsOpen] = useState(false);
     return (
-        <>
-            <Box sx={{ display: 'flex' }}>
-                <CssBaseline />
-                <AppBar component="nav" sx={{color: 'white'}}>
-                    <Toolbar>
-                        <IconButton
-                            color="inherit"
-                            aria-label="open drawer"
-                            edge="start"
-                            onClick={handleDrawerToggle}
-                            sx={{ mr: 2, display: { sm: 'none' } }}
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography
-                            variant="h6"
-                            component="div"
-                            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-                        >
-                            Logo
-                        </Typography>
-                        <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-                            {navItems.map((item) => (
-                                <Button key={item} sx={{ color: '#fff' }}>
-                                    {item}
-                                </Button>
-                            ))}
-                        </Box>
-                    </Toolbar>
-                </AppBar>
-                <nav>
-                    <Drawer
-                        container={container}
-                        variant="temporary"
-                        open={mobileOpen}
-                        onClose={handleDrawerToggle}
-                        ModalProps={{
-                            keepMounted: true, // Better open performance on mobile.
-                        }}
-                        sx={{
-                            display: { xs: 'block', sm: 'none' },
-                            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-                        }}
-                    >
-                        {drawer}
-                    </Drawer>
-                </nav>
-            </Box>
-        </>
+        <div className='w-full'>
+            <div className='w-full md:px-10 py-4 px-7 md:flex justify-between items-center fixed top-0 left-0'>
+                <div className='flex items-center gap-2'>
+                    <img src={logo} alt='logo' className='w-36' />
+                </div>
+
+                <div onClick={() => setIsOpen(!isOpen)} className='absolute right-8 top-6 md:hidden'>
+                    {
+                        isOpen ? <MenuIcon /> : <CloseOutlinedIcon />
+                    }
+                </div>
+
+                <ul className='md:flex pl-9 md:pl-0 md:items-center md:pb-0 pb-12'>
+                    {
+                        links.map((item, idx) => (
+                            <li className='my-7 md:my-0 md:ml-8'>
+                                <a href='/'
+                                    key={idx}
+                                >{item.name}</a>
+                            </li>
+                        ))
+                    }
+                </ul>
+                <div className='flex'>
+                    <div className='flex items-center justify-center'>
+                        <div>
+                            <LockOutlinedIcon />
+                        </div>
+                        <p>Login</p>
+                    </div>
+                    <div>
+                        <button className='py-1 px-3 md:ml-8 rounded md-static signupbutton'>
+                            Sign up for free
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div >
     )
 }
 
 export default Header
 
 
-// import React from 'react'
-// import { AppBar, IconButton, Toolbar, Typography, Menu, MenuItem, Button, Link, MenuIcon} from '@mui/material'
-// import Box from '@mui/material/Box';
-// import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
 
-// const pages = [
-
-//     { label: 'Home', path: '/' },
-//     { label: 'About', path: '/affiliate' },
-//     { label: 'Tech Network', path: '/technetwork' },
-//     { label: 'Blog', path: '/blog' },
-//     { label: 'Contact', path: '/contact' },
-// ];
-
-// const Header = () => {
-
-//     const [anchorElNav, setAnchorElNav] = React.useState(null);
-//     const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-//     const handleOpenUserMenu = (event) => {
-//         setAnchorElUser(event.currentTarget);
-//     };
-//     const handleOpenNavMenu = (event) => {
-//         setAnchorElNav(event.currentTarget);
-//     };
-
-//     const handleCloseNavMenu = () => {
-//         setAnchorElNav(null);
-//     };
-
-//     return (
-//         <AppBar position="static" sx={{ background: 'white' }}>
-//             <Toolbar>
-//                 <IconButton>
-//                     {/* <AssignmentIndIcon /> */}
-//                     <Typography>
-//                         Logo
-//                     </Typography>
-//                     <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-//                         {pages.map((page) => (
-//                             <Button
-//                                 key={page.label}
-//                                 component={Link}
-//                                 to={page.path}
-//                                 onClick={handleCloseNavMenu}
-//                                 sx={{ my: 2, color: 'white', display: 'block', fontFamily: '"Manrope", sans-serif' }}
-//                             >
-//                                 {page.label}
-//                             </Button>
-//                         ))}
-//                     </Box>
-
-//                     <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-//                         <IconButton
-//                             size="large"
-//                             aria-label="account of current user"
-//                             aria-controls="menu-appbar"
-//                             aria-haspopup="true"
-//                             onClick={handleOpenNavMenu}
-//                             color="inherit"
-//                         >
-//                             <MenuIcon />
-//                         </IconButton>
-//                         <Menu
-//                             id="menu-appbar"
-//                             anchorEl={anchorElNav}
-//                             anchorOrigin={{
-//                                 vertical: 'bottom',
-//                                 horizontal: 'left',
-//                             }}
-//                             keepMounted
-//                             transformOrigin={{
-//                                 vertical: 'top',
-//                                 horizontal: 'left',
-//                             }}
-//                             open={Boolean(anchorElNav)}
-//                             onClose={handleCloseNavMenu}
-//                             sx={{
-//                                 display: { xs: 'block', md: 'none' },
-//                             }}
-//                         >
-
-//                             {pages.map((page) => (
-//                                 <MenuItem key={page.label} onClick={handleCloseNavMenu}>
-//                                     <Typography textAlign="center">{page.label}</Typography>
-//                                 </MenuItem>
-//                             ))}
-//                         </Menu>
-//                     </Box>
-//                 </IconButton>
-//             </Toolbar>
-//         </AppBar>
-//     )
-// }
-
-// export default Header
